83aa0f7d9fea7e9ace54088d7cc6fc00681ba0e3
wends05 <wendellterence.dador-23@cpu.edu.ph>
2025-08-01 01:40:35 +0800

    [feat] added login ui and server action

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Archivo_Narrow } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const archivoNarrow = Archivo_Narrow({
+  variable: "--font-archivo-narrow",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Brat Utang Tracker",
-  description: "brat and it's the same but it's an utang tracking website so it's not",
+  description:
+    "brat and it's the same but it's an utang tracking website so it's not",
 };
 
 export default function RootLayout({
@@ -23,10 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en">
+      <body className={`${archivoNarrow.variable} antialiased`}>
         {children}
       </body>
     </html>
