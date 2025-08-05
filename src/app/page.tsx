4db15502c4487ffc1eaafc6810e181cf4fbf9343
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-08-06 01:07:52 +0800

    [style] added responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="relative flex h-screen w-full flex-col overflow-hidden">
-      <div className="absolute -right-50 -bottom-1 -z-10 h-full w-full">
+      <div className="absolute -right-10 md:-right-40 -bottom-1 -z-10 h-full w-full">
         <Image
           src={"/charli-landing.png"}
           alt="Charli Logo"
@@ -14,22 +14,22 @@ export default function Home() {
           className="bg-main object-cover object-[30%]"
         />
       </div>
-      <div className="flex h-full w-1/2 flex-col gap-4 p-30">
-        <p className="text-8xl blur-[1px]">
+      <div className="flex h-full w-1/2 flex-col gap-4 p-14 lg:p-20">
+        <p className="text-3xl sm:text-6xl blur-[1px] lg:text-7xl">
           brat and it&apos;s the same but it&apos;s an utang tracking app so
           it&apos;s not
         </p>
-        <p className="text-white blur-[0.5px]">
+        <p className="text-2xl text-white blur-[0.5px]">
           because financial management is fierce
         </p>
-        <div className="flex w-full gap-5 text-white blur-[0.7px]">
+        <div className="flex w-full gap-5 text-white blur-[0.7px] text-xl">
           <Link href={"/login"}>
-            <Button variant={"outline"} className="bg-main">
+            <Button variant={"outline"} className="bg-main text-xl">
               Login
             </Button>
           </Link>
           <Link href={"/signup"}>
-            <Button variant={"outline"} className="bg-main">
+            <Button variant={"outline"} className="bg-main text-xl">
               Sign Up
             </Button>
           </Link>
